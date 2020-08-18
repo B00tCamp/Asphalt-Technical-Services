898551ae60c2c6ab0d16791189ea02ccb8ff5120
Jovan de Beer <45359245+B00tCamp@users.noreply.github.com>
2020-08-18 11:21:46 +0200

    Initialized Node and added Express
    Added node and express, separated javascript for the site and the server javascript. updated all html pages with the new javaScript.js file.

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+const express = require("express");
+const app = express();
 
-// Get the navbar
-var navbar = document.getElementById("navBar");
-var wrapping = document.getElementById("wrapping")
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-    wrapping.classList.add("stickyMode");
-  } else {
-    navbar.classList.remove("sticky");
-    wrapping.classList.remove("stickyMode");
-  }
-}
-
-//GoogleMap
-window.onload = function myMap() {
-var mapProp= {
-  center:new google.maps.LatLng(-25.909469,28.000803),
-  zoom:15,
-};
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
+app.listen(3000, function(){
+  console.log("Server started on port 3000")
+});
